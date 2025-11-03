842d255b9d54c4731b6af8bfd3579416b74a0e45
Khallid Nurse <doctormen131@outlook.com>
2025-11-02 23:10:31 -0500

    Prod:

--- docs/config.js
+++ docs/config.js
@@ -1,8 +1,8 @@
 window.appConfig = {
   // Replace these with your live URLs
-  stripeCheckoutUrl: 'https://buy.stripe.com/your-checkout-link',
-  stripeExpressUrl: 'https://buy.stripe.com/your-express-checkout-link',
-  stripeMonthlyUrl: 'https://buy.stripe.com/your-monthly-checkout-link',
+  stripeCheckoutUrl: 'https://buy.stripe.com/cNi7sE9Yi6vxfO4gwP0kE01',
+  stripeExpressUrl: 'https://buy.stripe.com/9B6cMY4DY9HJcBS4O70kE00',
+  stripeMonthlyUrl: 'https://buy.stripe.com/6oU14gfiC7zB7hygwP0kE03',
   googleFormUrl: 'https://docs.google.com/forms/d/e/GOOGLE_FORM_URL_HERE/viewform',
   calendlyUrl: 'https://calendly.com/your-15min-link',
   slotsRemaining: 3
